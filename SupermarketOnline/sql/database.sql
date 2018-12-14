bd3e03652a5e4bbc2ce031b235f148252d2272bf
huhuiyu <1069306849@qq.com>
2018-12-14 11:30:09 +0800

    数据库更新

--- SupermarketOnline/sql/database.sql
+++ SupermarketOnline/sql/database.sql
@@ -27,3 +27,36 @@ create table TbTokenInfo
   lastupdate timestamp comment '令牌环最后更新时间',
   constraint pkTbTokenInfo primary key(token,infokey)
 );
+
+/* 后台管理员用户 */
+create table TbAdminUser
+(
+  auid int auto_increment primary key comment '主键',
+  username varchar(20) unique not null comment '用户名',
+  password varchar(20) not null comment '密码',
+  nickname varchar(50) not null comment '昵称',
+  isEnable enum('y','n') default 'y' comment '是否启用，y:默认，启用，n:停用',
+  lastupdate timestamp default now() comment '最后更新时间'
+);
+
+/* 商品大分类 */
+create table TbType
+(
+  tid int auto_increment primary key comment '主键',
+  typeName varchar(8) unique not null comment '分类名称',
+  typeInfo varchar(200) not null comment '分类描述',
+  isEnable enum('y','n') default 'y' comment '是否启用，y:默认，启用，n:停用',
+  lastupdate timestamp default now() comment '最后更新时间'
+);
+
+/* 商品小分类 */
+create table TbSubType
+(
+  stid int auto_increment primary key comment '主键',
+  tid int not null comment '外键，所属大类',
+  subName varchar(8) not null comment '分类名称',
+  subInfo varchar(200) not null comment '分类描述',
+  isEnable enum('y','n') default 'y' comment '是否启用，y:默认，启用，n:停用',
+  lastupdate timestamp default now() comment '最后更新时间'
+);
+
--- SupermarketOnline/sql/init.sql
+++ SupermarketOnline/sql/init.sql
@@ -5,6 +5,10 @@ truncate table TbConfig;
 truncate table TbToken;
 truncate table TbTokenInfo;
 
+truncate table TbAdminUser;
+truncate table TbType;
+truncate table TbSubType;
+
 /*token过期时间配置*/
 insert into TbConfig(configKey,configValue) values('token.timeout','30');
 
@@ -12,3 +16,7 @@ insert into TbConfig(configKey,configValue) values('token.timeout','30');
 select configKey,configValue,lastupdate from TbConfig;
 select token,lastupdate from TbToken;
 select token,infokey,info,lastupdate from TbTokenInfo;
+
+select auid,username,password,nickname,isEnable,lastupdate from TbAdminUser;
+select tid,typeName,typeInfo,isEnable,lastupdate from TbType;
+select stid,tid,subName,subInfo,isEnable,lastupdate from TbSubType;
